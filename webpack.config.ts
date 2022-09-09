cfa922b6240b7e02b694192db6473a4c41877657
Arseniy-II <Arseniy.public@gmail.com>
2022-09-09 18:00:20 +0200

    chore: add jest configuration for testing

--- webpack.config.ts
+++ webpack.config.ts
@@ -50,6 +50,14 @@ const webpackConfig = (): Configuration => ({
                     },
                 ],
             },
+            {
+                test: /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
         ],
     },
     devServer: {
