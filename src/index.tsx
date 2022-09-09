665a8427dbd80a9b8bad949f339ef0030a7ac41e
Arseniy-II <Arseniy.public@gmail.com>
2022-09-09 14:29:19 +0200

    feat: used NumberInputComponent, small style update and small refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CurrencyConverterComponent from './Components/CurrencyConverterComponent';
+import './styles.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <CurrencyConverterComponent />
     </React.StrictMode>,
     document.getElementById('app')
 );
