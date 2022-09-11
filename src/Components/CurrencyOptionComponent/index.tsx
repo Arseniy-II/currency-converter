76f8311802a546b4123de1da794e4146a41eee30
Arseniy-II <Arseniy.public@gmail.com>
2022-09-11 18:17:33 +0200

    feat: improve CurrencySelectComponent

---
+++ src/Components/CurrencyOptionComponent/index.tsx
@@ -0,0 +1,20 @@
+import CurrencyFlag from 'react-currency-flags';
+import styles from './styles.scss';
+import React, {Fragment} from 'react';
+
+interface currencyOptionProps {
+    currency: string;
+}
+
+const CurrencyOptionComponent: React.FC<currencyOptionProps> = (props) => {
+    const {currency} = props;
+    console.log(styles);
+    return (
+        <Fragment>
+            <CurrencyFlag currency={currency } className={styles.flagIcon}/>
+            {currency}
+        </Fragment>
+    );
+};
+
+export default CurrencyOptionComponent;
--- src/Components/CurrencySelectComponent/getFlagOptionsForSelect.tsx
+++ src/Components/CurrencySelectComponent/getFlagOptionsForSelect.tsx
@@ -1,25 +1,12 @@
-import React, {Fragment} from 'react';
-import flags from 'country-flag-icons/react/3x2';
-import countryToCurrency from 'country-to-currency';
-import styles from './styles.scss';
+import React from 'react';
+import CurrencyOptionComponent from '../CurrencyOptionComponent';
+import {CurrencyType} from './currencies';
 
-const getFlagOptionsForSelect = (countries: string[]): { value: string, label: React.ReactElement }[] => {
-    const flagOptionsForSelect = [];
-    countries.forEach((countryCode) => {
-        const ReactSVGIcon = flags[countryCode];
-        let currency = countryToCurrency[ countryCode ];
-        if (countryCode === 'EU') {
-            currency = 'EUR';
-        }
-        if (ReactSVGIcon && currency) {
-            const CurrencyComponent = <Fragment><ReactSVGIcon className={styles.flagIcon}/>{currency}</Fragment>;
-            flagOptionsForSelect.push({
-                value: currency,
-                label: CurrencyComponent
-            });
-        }
-    });
-    return flagOptionsForSelect;
+const getFlagOptionsForSelect = (currencies: CurrencyType[]): { value: string, label: React.ReactElement }[] => {
+    return currencies.map(({cc}) => ({
+        value: cc,
+        label: <CurrencyOptionComponent currency={cc}/>
+    }));
 };
 
 export default getFlagOptionsForSelect;
